d5839ad3960a2449161a3b2173036970187bcdad
Atakos254 <emmanuelatakos@gmail.com>
2022-05-17 16:06:16 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,11 +22,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
-		if (str == NULL)
+		if (str != NULL)
 		{
-			str = "(nil)";
+			printf("%s", str);
 		}
-		printf("%s", str);
 		if (i != (n - 1))
 		{
 			printf("%s", separator);
