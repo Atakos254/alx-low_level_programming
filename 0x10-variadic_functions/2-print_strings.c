4936b8f3f17bc5d57fa346e8eee2035a8b513754
Atakos254 <emmanuelatakos@gmail.com>
2022-05-17 15:16:59 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,6 +8,7 @@
  * Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
+
 {
 	va_list valist;
 	unsigned int i;
@@ -32,6 +33,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 	}
 	printf("\n");
-
 	va_end(valist);
 }
