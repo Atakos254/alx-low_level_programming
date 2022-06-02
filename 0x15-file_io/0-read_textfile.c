eae5161448a981e8974ceb17e5d1c67c89db968c
Atakos254 <emmanuelatakos@gmail.com>
2022-06-02 14:21:54 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
  * Return: the number of letters printed, or 0 if it failed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+
 	int i, y;
+
 	char *buf;
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
